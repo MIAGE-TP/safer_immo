954be6878a37dc8b849b8179fd54e59db6b7b1dc
HeavenSon <hodethierry@gmail.com>
2022-12-20 16:52:54 +0100

    Homepage template done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    // #[Route('/home', name: 'app_home')]
     public function index()
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
