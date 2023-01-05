84fddfcd2c9ab2fd017627a6e18158a2cefc9068
HEAVENSON\zeush <hodethierry@gmail.com>
2023-01-05 21:15:26 +0100

    Use laragon

--- migrations/Version20230105200226.php
+++ migrations/Version20230105200226.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221227154935 extends AbstractMigration
+final class Version20230105200226 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,18 +22,23 @@ final class Version20221227154935 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE city (id INT AUTO_INCREMENT NOT NULL, department_id INT NOT NULL, zip_code INT DEFAULT NULL, name VARCHAR(255) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_2D5B0234AE80F5DF (department_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE department (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE good (id INT AUTO_INCREMENT NOT NULL, offertype_id INT NOT NULL, goodcategory_id INT NOT NULL, user_id INT NOT NULL, city_id INT NOT NULL, reference VARCHAR(255) NOT NULL, intitule VARCHAR(255) DEFAULT NULL, descriptif VARCHAR(510) DEFAULT NULL, localisation VARCHAR(510) DEFAULT NULL, surface VARCHAR(255) NOT NULL, price VARCHAR(255) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_6C844E92E52A1CB1 (offertype_id), INDEX IDX_6C844E92AFB306D3 (goodcategory_id), INDEX IDX_6C844E92A76ED395 (user_id), INDEX IDX_6C844E928BAC62AF (city_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE fav (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, good_id INT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_769BE06FA76ED395 (user_id), INDEX IDX_769BE06F1CF98C70 (good_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE good (id INT AUTO_INCREMENT NOT NULL, offertype_id INT NOT NULL, goodcategory_id INT NOT NULL, user_id INT NOT NULL, city_id INT NOT NULL, reference VARCHAR(255) NOT NULL, intitule VARCHAR(255) DEFAULT NULL, descriptif VARCHAR(510) DEFAULT NULL, localisation VARCHAR(510) DEFAULT NULL, surface VARCHAR(255) NOT NULL, price VARCHAR(255) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', slug VARCHAR(1020) NOT NULL, hidden TINYINT(1) NOT NULL, unit VARCHAR(255) NOT NULL, street VARCHAR(1020) DEFAULT NULL, INDEX IDX_6C844E92E52A1CB1 (offertype_id), INDEX IDX_6C844E92AFB306D3 (goodcategory_id), INDEX IDX_6C844E92A76ED395 (user_id), INDEX IDX_6C844E928BAC62AF (city_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE good_category (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE news_letter (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, fav JSON NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_2AB2D7EA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE offer_galery (id INT AUTO_INCREMENT NOT NULL, good_id INT NOT NULL, name VARCHAR(1275) NOT NULL, path VARCHAR(2550) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_D20824811CF98C70 (good_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE offer_type (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE project_type (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', is_verified TINYINT(1) NOT NULL, token VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE city ADD CONSTRAINT FK_2D5B0234AE80F5DF FOREIGN KEY (department_id) REFERENCES department (id)');
+        $this->addSql('ALTER TABLE fav ADD CONSTRAINT FK_769BE06FA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE fav ADD CONSTRAINT FK_769BE06F1CF98C70 FOREIGN KEY (good_id) REFERENCES good (id)');
         $this->addSql('ALTER TABLE good ADD CONSTRAINT FK_6C844E92E52A1CB1 FOREIGN KEY (offertype_id) REFERENCES offer_type (id)');
         $this->addSql('ALTER TABLE good ADD CONSTRAINT FK_6C844E92AFB306D3 FOREIGN KEY (goodcategory_id) REFERENCES good_category (id)');
         $this->addSql('ALTER TABLE good ADD CONSTRAINT FK_6C844E92A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE good ADD CONSTRAINT FK_6C844E928BAC62AF FOREIGN KEY (city_id) REFERENCES city (id)');
+        $this->addSql('ALTER TABLE news_letter ADD CONSTRAINT FK_2AB2D7EA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE offer_galery ADD CONSTRAINT FK_D20824811CF98C70 FOREIGN KEY (good_id) REFERENCES good (id)');
     }
 
@@ -41,15 +46,20 @@ final class Version20221227154935 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE city DROP FOREIGN KEY FK_2D5B0234AE80F5DF');
+        $this->addSql('ALTER TABLE fav DROP FOREIGN KEY FK_769BE06FA76ED395');
+        $this->addSql('ALTER TABLE fav DROP FOREIGN KEY FK_769BE06F1CF98C70');
         $this->addSql('ALTER TABLE good DROP FOREIGN KEY FK_6C844E92E52A1CB1');
         $this->addSql('ALTER TABLE good DROP FOREIGN KEY FK_6C844E92AFB306D3');
         $this->addSql('ALTER TABLE good DROP FOREIGN KEY FK_6C844E92A76ED395');
         $this->addSql('ALTER TABLE good DROP FOREIGN KEY FK_6C844E928BAC62AF');
+        $this->addSql('ALTER TABLE news_letter DROP FOREIGN KEY FK_2AB2D7EA76ED395');
         $this->addSql('ALTER TABLE offer_galery DROP FOREIGN KEY FK_D20824811CF98C70');
         $this->addSql('DROP TABLE city');
         $this->addSql('DROP TABLE department');
+        $this->addSql('DROP TABLE fav');
         $this->addSql('DROP TABLE good');
         $this->addSql('DROP TABLE good_category');
+        $this->addSql('DROP TABLE news_letter');
         $this->addSql('DROP TABLE offer_galery');
         $this->addSql('DROP TABLE offer_type');
         $this->addSql('DROP TABLE project_type');
