c522b4338bdfe66da45fbe2f6e637fccbd5c7ef5
HEAVENSON\zeush <hodethierry@gmail.com>
2023-01-08 17:02:44 +0100

    Fixtures for defautl user done

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,14 +7,17 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\GoodCategory;
 use App\Entity\OfferType;
+use App\Entity\User;
 use Faker\Factory;
 use Faker\Generator;
+
 
 class AppFixtures extends Fixture
 {
     private Generator $faker;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->faker = Factory::create('fr_FR');
     }
 
@@ -41,6 +44,26 @@ class AppFixtures extends Fixture
             $manager->persist($department);
         }
 
+        $admin = [
+            [
+                'email' => 'admin@safer.com',
+                'role' => ["ROLE_ADMIN"],
+                'firstname' => 'safer',
+                'lastname' => 'safer',
+                'isVerfied' => 1,
+                'password' => '$2y$13$NdLB1IlFY6YKoScPi7bWh.h3kXy7A3p.2JxLjZyXYXKF1qc2xO96C'
+            ]
+        ];
+
+        $user = new User();
+        $user->setEmail($admin[0]['email']);
+        $user->setRoles($admin[0]['role']);
+        $user->setFirstname($admin[0]['firstname']);
+        $user->setLastname($admin[0]['lastname']);
+        $user->setIsVerified($admin[0]['isVerfied']);
+        $user->setPassword($admin[0]['password']);
+        $manager->persist($user);
+        
         $manager->flush();
     }
 }
